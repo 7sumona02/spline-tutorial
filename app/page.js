06edf8e69da8a01d8f39f58e081dffb56b73ba33
7sumona02 <hawkingsumona8@gmail.com>
2024-08-30 22:04:55 +0530

    added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
-import Spline from '@splinetool/react-spline';
+import Earth from '@/components/Earth'
+import React from 'react'
 
-export default function App() {
+const page = () => {
   return (
-    <Spline scene="https://prod.spline.design/Tm52I2Es6pOQMjNG/scene.splinecode" />
-  );
+    <div className='h-screen w-full flex justify-center flex-col items-center p-10'>
+      <h1 className='text-6xl font-bold'>Planet Earth</h1>
+      <p className='max-w-xl text-center text-sm mt-10 mb-10'>Earth is a rocky, terrestrial planet that is the third-closest planet to the sun and the fifth-largest in our solar system. It has a solid surface with mountains, valleys, canyons, and plains, and is covered by 70% water, making it an ocean planet. Earth&apos;s atmosphere is made up of mostly nitrogen and oxygen, with trace amounts of other gases like carbon dioxide and water vapor.</p>
+      <Earth />
+    </div>
+  )
 }
+
+export default page
