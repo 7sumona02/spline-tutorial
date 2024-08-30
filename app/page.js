0c53e16bfe71b0966951166b420e5407ca9ca8ee
7sumona02 <hawkingsumona8@gmail.com>
2024-08-30 21:50:41 +0530

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/Tm52I2Es6pOQMjNG/scene.splinecode" />
+  );
+}
